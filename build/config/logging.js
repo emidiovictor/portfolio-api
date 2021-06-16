efb0e0ad291cc059ab76a2ebe134f511435c47af
Victor Emidio <victor.emidio@rockcontent.com>
2021-06-16 20:21:10 -0300

    first commit

--- build/config/logging.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getTimeStamp = function () {
-    return new Date().toISOString();
-};
-var info = function (namespace, message, object) {
-    console.info("[" + getTimeStamp() + "] [INFO] [" + namespace + " " + message + "]", object);
-};
-exports.default = { info: info };
